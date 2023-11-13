3e7e9ad8a5542e526129c6b224ac983cb4f5f6b8
ojama <o.jamali2016@gmail.com>
2023-11-13 21:29:06 +0330

    Initial project

---
+++ Services/Catalog/Catalog.Api/Models/Product.cs
@@ -0,0 +1,14 @@
+﻿using MongoDB.Bson;
+using MongoDB.Bson.Serialization.Attributes;
+
+namespace Catalog.Api.Models
+{
+    public class Product
+    {
+        [BsonId]
+        [BsonRepresentation(BsonType.ObjectId)]
+        public string Id { get; set; }
+        public string Name { get; set; }
+        public double Price { get; set; }
+    }
+}
