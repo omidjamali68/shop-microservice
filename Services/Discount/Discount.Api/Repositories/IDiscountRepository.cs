b475454d8e5a640cd233c5aceedf643fa53a1c3d
omidjamali68 <o.jamali2016@gmail.com>
2023-12-04 16:31:43 +0330

    Add discount project

---
+++ Services/Discount/Discount.Api/Repositories/IDiscountRepository.cs
@@ -0,0 +1,12 @@
+﻿using Discount.Api.Entities;
+
+namespace Discount.Api.Repositories
+{
+    public interface IDiscountRepository
+    {
+        Task<Coupon> GetDiscount(string productsId);
+        Task<bool> CreateDiscount(Coupon copoun);
+        Task<bool> UpdateDiscount(Coupon coupon);
+        Task<bool> DeleteDiscount(string productId);
+    }
+}
