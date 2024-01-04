e37eb6e159897aff4deaf84c00de0cbe9dac9936
omidjamali68 <o.jamali2016@gmail.com>
2024-01-04 11:52:11 +0330

    Make relation between ordering layer and Implement order entity

---
+++ Services/Ordering/Ordering.Domain/Entities/Order.cs
@@ -0,0 +1,19 @@
+﻿using Ordering.Domain.Common;
+
+namespace Ordering.Domain.Entities
+{
+    public class Order : EntityBase
+    {
+        public string UserName { get; set; }
+        public decimal TotalPrice { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string EmailAddress { get; set; }
+        public string Country { get; set; }
+        public string City { get; set; }
+        public string BankName { get; set; }
+        public string RefCode { get; set; }
+        public byte PaymentMethod { get; set; }
+
+    }
+}
